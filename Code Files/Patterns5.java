03e2c25afec68d5f9a446ce9c754679959356ba2
Rishab Khar <kharrishab@gmail.com>
2023-12-09 20:57:00 +0530

    Day1

---
+++ Code Files/Patterns5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Patterns5 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);      
+        System.out.print("Enter number- ");  
+        int N = sc.nextInt();
+        for(int i=1; i<=N; i++)
+        {
+            for(int j=N; j>=i; j--)
+            {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
